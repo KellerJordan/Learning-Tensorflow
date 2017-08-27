ecd4eb511d893da4ef71c7c6aac4d850f744b222
Keller Jordan <kellerjordan1998@gmail.com>
2017-08-26 20:40:40 -0700

    add timing, multiple cost functions

--- ANNs/main.py
+++ ANNs/main.py
@@ -4,19 +4,11 @@ from pprint import pprint
 
 if __name__ == '__main__':
 
-    # net = network.Network([5, 3, 2])
-    # X = np.array([
-    #     [1, 1, 1, 1, 1],
-    #     [5, 5, 5, 5, 5],
-    # ])
-    # y = np.array([
-    #     0,
-    #     1,
-    # ])
-    # training_data = list(zip(X, y))
-    # net.SGD(training_data, 30, 2, 3.0, test_data=training_data)
-
-    net = network.Network([784, 30, 10])
     import mnist_loader
     training_data, validation_data, test_data = mnist_loader.load_data()
-    net.SGD(training_data, 15, 5, 3.0, test_data=test_data)
+    net = network.Network([784, 100, 10], cost=network.CrossEntropyCost)
+    net.large_weight_initializer()
+    net.SGD(
+        training_data, 15, 5, .5,
+        evaluation_data=test_data,
+        monitor_evaluation_accuracy=True)
--- ANNs/network.py
+++ ANNs/network.py
@@ -1,33 +1,41 @@
 import random
 import numpy as np
 
+import sys
+from pprint import pprint
+
 
 class Network:
 
     SEED = 3
 
-    def __init__(self, sizes):
+    def __init__(self, sizes, cost):
         self.num_layers = len(sizes)
         np.random.seed(self.SEED)
         self.biases = [np.random.randn(y) for y in sizes[1:]]
         self.weights = [np.random.randn(y, x) for x, y in zip(sizes[:-1], sizes[1:])]
+        self.cost_derivative = cost
+
+    def large_weight_initializer(self):
+        pass
 
     def feedforward(self, x, save_activations=False):
         a = x
         if save_activations:
             activations = [a]
         for b, w in zip(self.biases, self.weights):
-            a = sigmoid(np.dot(w, a) + b)
+            a = sigmoid(np.matmul(w, a) + b)
             if save_activations:
                 activations.append(a)
         return activations if save_activations else a
 
+    # uses denominator layout for matrix calculus
     def backprop(self, x, y):
         # feedforward, saving activations
         activations = self.feedforward(x, save_activations=True)
         h = activations[-1]
         # backpropagation
-        del_h_wrt_z = h * (1 - h)
+        del_h_wrt_z = sigmoid_prime(h)
         del_E_wrt_z = self.cost_derivative(h, y) * del_h_wrt_z
         nabla_b = [np.zeros(b.shape) for b in self.biases]
         nabla_w = [np.zeros(w.shape) for w in self.weights]
@@ -35,34 +43,39 @@ class Network:
         nabla_w[-1] = np.outer(del_E_wrt_z, activations[-2])
         for l in range(2, self.num_layers):
             a = activations[-l]
-            del_E_wrt_a = np.matmul(del_E_wrt_z[np.newaxis], self.weights[-l+1])[0]
-            del_a_wrt_z = a * (1 - a)
+            del_E_wrt_a = np.matmul(self.weights[-l+1].transpose(), del_E_wrt_z)
+            del_a_wrt_z = sigmoid_prime(a)
             del_E_wrt_z = del_E_wrt_a * del_a_wrt_z
             nabla_b[-l] = del_E_wrt_z
             nabla_w[-l] = np.outer(del_E_wrt_z, activations[-l-1])
         return (nabla_b, nabla_w)
-
-    def cost_derivative(self, h, y):
-        return h - y
 
     def evaluate(self, test_data):
         test_results = [(np.argmax(self.feedforward(x)), y)
                         for x, y in test_data]
         return sum(int(h == y) for h, y in test_results)
 
-    def SGD(self, data_train, epochs, minibatch_size, eta, test_data=None):
-        if test_data:
-            n_test = len(test_data)
+    def SGD(self, data_train, epochs, minibatch_size, eta,
+            evaluation_data=None,
+            monitor_evaluation_accuracy=True):
+        from datetime import datetime
+        time_start = datetime.now()
+        if evaluation_data:
+            n_test = len(evaluation_data)
         n_train = len(data_train)
         for j in range(epochs):
             random.shuffle(data_train)
             for k in range(0, n_train, minibatch_size):
                 minibatch = data_train[k:k+minibatch_size]
                 self.minibatch_update(minibatch, eta)
-            if test_data:
-                print('Epoch {}: {} / {}.'.format(j+1, self.evaluate(test_data), n_test))
+            if evaluation_data:
+                print('Epoch {}: {} / {}.'
+                      .format(j+1, self.evaluate(evaluation_data), n_test))
             else:
                 print('Epoch {} complete.'.format(j))
+        time_end = datetime.now()
+        time_delta = str(time_end - time_start).rsplit('.', 1)[0]
+        print(time_delta)
 
     def minibatch_update(self, minibatch, eta):
         nabla_b = [np.zeros(b.shape) for b in self.biases]
@@ -74,6 +87,18 @@ class Network:
         self.weights = [w - (eta/len(minibatch))*nw for w, nw in zip(self.weights, nabla_w)]
         self.biases = [b - (eta/len(minibatch))*nb for b, nb in zip(self.biases, nabla_b)]
 
-
+## math functions ---------------------------------------------------------------------------------
 def sigmoid(z):
-    return 1.0 / (1.0 + np.exp(-z))
+    # return z * (z > 0) # ReLU
+    return 1.0 / (1.0 + np.exp(-z)) # sigmoid
+
+def sigmoid_prime(a):
+    # return 1 * (a > 0) # ReLU
+    return a * (1 - a) # sigmoid
+
+## cost functions ---------------------------------------------------------------------------------
+def QuadraticCost(h, y):
+        return h - y
+
+def CrossEntropyCost(h, y):
+    return (h - y) / (h * (1 - h))
