4ba21695b66af10add06d73fd232006b48c1e75b
Keller Jordan <kellerjordan1998@gmail.com>
2017-08-26 21:07:34 -0700

    double epochs

--- ANNs/main.py
+++ ANNs/main.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     net = network.Network([784, 100, 10])
     net.large_weight_initializer()
     net.SGD(
-        training_data, 15, 5, .5,
+        training_data, 30, 5, .5,
         cost=network.CrossEntropyCost,
         normalization=network.L2Normalizer(lmbda=.1),
         evaluation_data=test_data,
